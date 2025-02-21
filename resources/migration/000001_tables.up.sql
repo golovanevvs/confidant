26d49996f364a1f8f71086aba93a6032671be2da
golovanevvs <lony@inbox.ru>
2025-02-22 01:11:59 +0300

    Sync: dev min 6: begin migration DB

---
+++ resources/migration/000001_tables.up.sql
@@ -0,0 +1,16 @@
+begin;
+
+CREATE TABLE account(
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(250) NOT NULL UNIQUE,
+    password_hash VARCHAR(250) NOT NULL
+);
+
+CREATE TABLE groups(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(250),
+    account_id INT,
+    FOREIGN KEY (account_id) REFERENCES account (id)
+);
+
+commit;
